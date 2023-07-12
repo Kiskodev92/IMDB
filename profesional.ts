4d16f87502be8d91dd2a39abc2c14dc3f7960157
Kiskotkd <fjgp92@hotmail.com>
2023-07-12 09:57:01 +0200

    profesional

---
+++ profesional.ts
@@ -0,0 +1,11 @@
+class Profesional{
+    public name: string;
+    public age: number;
+    public weight: number;
+    public height: number;
+    public isRetired: boolean;
+    public nationality: string;
+    public oscarsNumber: number;
+    public profession: string;
+    constructor()
+}
