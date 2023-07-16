af9132919563341eaaca89be27c568fed7674bd8
Kiskotkd <fjgp92@hotmail.com>
2023-07-16 11:50:29 +0200

    imdb resuelto

--- imdbTest.ts
+++ imdbTest.ts
@@ -5,6 +5,6 @@ let peli1 = new Movie ("Hercules", 1999, "griega", "infantil");
 let peli2 = new Movie ("Deadpool", 2016, "USA", "superheroes");
 let peli3 = new Movie ("Diario de Noa", 2004, "USA", "Romance");
 let arrpeli =[peli1, peli2, peli3];
-let milista = new imdb(arrpeli);
+let milista : imdb = new imdb(arrpeli);
 
 console.log(milista);
