f949739ec27468305a46cd7b31644e97b7b47eee
Kiskotkd <fjgp92@hotmail.com>
2023-07-17 10:16:26 +0200

    avance

--- imdb.js
+++ imdb.js
@@ -1,10 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.imdb = void 0;
+var fs = require('fs');
 var imdb = /** @class */ (function () {
-    function imdb(peli) {
-        this.peli = peli;
+    function imdb() {
+        this.peli = [];
     }
+    imdb.prototype.escribirEnFicheroJSON = function (nombreFichero) {
+        var newJson = JSON.stringify(this.peli);
+        fs.writeFileSync(nombreFichero, newJson);
+    };
+    imdb.prototype.obtenerInstanciaIMDB = function (nombreFichero) {
+        var dato = fs.readFileSync(nombreFichero, "utf8");
+        var obj = JSON.parse(dato);
+        var objDato = Object.assign(new imdb(), obj);
+        return objDato;
+    };
     return imdb;
 }());
 exports.imdb = imdb;
+var dvd = new imdb();
+var objImdb = JSON.stringify(dvd);
+fs.writeFileSync("imdbBBDD.json", objImdb);
+var data = fs.readFileSync("imdbBBDD.json", "utf8");
+console.log(data);
+var insImdb = JSON.parse(data);
+console.log(insImdb);
--- imdb.ts
+++ imdb.ts
@@ -1,7 +1,33 @@
 import { Movie } from "./movie";
+let fs = require('fs');
+
 export class imdb{
     public peli : Movie[];
-    constructor(peli : Movie[]){
-        this.peli = peli;
+    constructor(){
+        this.peli = [];
+    }
+    escribirEnFicheroJSON(nombreFichero : string){
+        let newJson = JSON.stringify(this.peli);
+        fs.writeFileSync(nombreFichero, newJson)
+        ;
+    }
+    obtenerInstanciaIMDB(nombreFichero : string) : imdb {
+        let dato =fs.readFileSync(nombreFichero ,"utf8")
+        let obj=JSON.parse(dato)
+        let objDato = Object.assign(new imdb(),obj)
+        return objDato;
     }
 }
+let dvd : imdb = new imdb();
+
+let objImdb = JSON.stringify(dvd)
+
+fs.writeFileSync("imdbBBDD.json",objImdb)
+
+let data = fs.readFileSync("imdbBBDD.json", "utf8")
+
+console.log(data);
+
+let insImdb = JSON.parse(data)
+
+console.log(insImdb);
